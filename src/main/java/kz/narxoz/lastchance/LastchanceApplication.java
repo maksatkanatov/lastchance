1ebf6068cbfc0ffb2419f7446c4fa54019917dd0
Max <maksatkanat@gmail.com>
2022-11-26 11:22:22 +0600

    init

---
+++ src/main/java/kz/narxoz/lastchance/LastchanceApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.lastchance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastchanceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LastchanceApplication.class, args);
+  }
+
+}
